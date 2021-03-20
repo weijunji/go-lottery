99f0666e24d32527b21476d7a6b66c00ed07419b
kdjlyy <kdjlyy@qq.com>
2021-03-20 11:27:46 +0800

    modified table format

--- internal/info/info.go
+++ internal/info/info.go
@@ -2,17 +2,18 @@ package info
 
 import (
 	"context"
+	"net/http"
+	"strconv"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/weijunji/go-lottery/pkgs/utils"
 	myProto "github.com/weijunji/go-lottery/proto"
-	"net/http"
-	"strconv"
-	"time"
 )
-//
-const timeFormat = "2006-01-02 15:04:05"
+
+//const timeFormat = "2006-01-02 15:04:05"
 var (
 	rdb = utils.GetRedis()
 )
@@ -24,17 +25,17 @@ func init() {
 //load routers
 func LoadRouter(r *gin.RouterGroup) {
 	{
-		r.GET("/lottery_info", 	LotteryInfo)
-		r.GET("/awards_info",  	AwardsInfo)
-		r.GET("/win_info", 		WinInfo)
-		r.GET("/draw_times", 	DrawTimes)
+		r.GET("/lottery_info", LotteryInfo)
+		r.GET("/awards_info", AwardsInfo)
+		r.GET("/win_info", WinInfo)
+		r.GET("/draw_times", DrawTimes)
 	}
 }
 
 // Users [...]
 type Users struct {
 	ID          uint64    `gorm:"primaryKey;column:id;type:bigint unsigned;not null" json:"-"`
-	AccessToken string    `gorm:"index:access_token;column:access_token;type:varchar(128)" json:"-"`
+	AccessToken string    `gorm:"column:access_token;type:varchar(128)" json:"-"`
 	TokenType   int64     `gorm:"column:token_type;type:bigint" json:"-"`
 	Role        int64     `gorm:"column:role;type:bigint" json:"role"`
 	CreatedAt   time.Time `gorm:"column:created_at;type:datetime(3)" json:"created_at"`
@@ -61,8 +62,8 @@ func (m *Lotteries) TableName() string {
 //AwardInfos: struct for award_infos
 type AwardInfos struct {
 	ID          uint64    `gorm:"primaryKey;column:id;type:bigint unsigned;not null" json:"-"`
-	Lottery     uint64    `gorm:"index:fk_award_infos_fkey;column:lottery;type:bigint unsigned" json:"lottery"`
-	Lotteries   Lotteries `gorm:"joinForeignKey:lottery;foreignKey:id" json:"lotteries_list"`
+	Lottery     uint64    `gorm:"column:lottery;type:bigint unsigned;not null" json:"lottery"`
+	Lotteries   Lotteries `gorm:"foreignkey:Lottery;association_foreignkey:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Name        string    `gorm:"column:name;type:varchar(32)" json:"name"`
 	Type        int64     `gorm:"column:type;type:bigint" json:"type"`
 	Description string    `gorm:"column:description;type:text" json:"description"`
@@ -79,9 +80,9 @@ func (m *AwardInfos) TableName() string {
 // Awards [...]
 type Awards struct {
 	Award      uint64     `gorm:"primaryKey;column:award;type:bigint unsigned;not null" json:"-"`
-	AwardInfos AwardInfos `gorm:"joinForeignKey:award;foreignKey:id" json:"award_infos_list"`
+	AwardInfos AwardInfos `gorm:"foreignkey:Award;association_foreignkey:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Lottery    uint64     `gorm:"index:idx_awards_lottery;column:lottery;type:bigint unsigned" json:"lottery"`
-	Lotteries  Lotteries  `gorm:"joinForeignKey:lottery;foreignKey:id" json:"lotteries_list"`
+	Lotteries  Lotteries  `gorm:"foreignkey:Lottery;association_foreignkey:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Reamin     int64      `gorm:"column:reamin;type:bigint" json:"reamin"`
 }
 // TableName get sql table name.
@@ -93,27 +94,27 @@ func (m *Awards) TableName() string {
 type WinningInfos struct {
 	ID         uint64     `gorm:"primaryKey;column:id;type:bigint unsigned;not null" json:"-"`
 	User       uint64     `gorm:"index:idx_winning_infos_user;column:user;type:bigint unsigned" json:"user"`
-	Users      Users      `gorm:"joinForeignKey:user;foreignKey:id" json:"users_list"`
+	Users      Users      `gorm:"foreignkey:User;association_foreignkey:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Award      uint64     `gorm:"index:idx_winning_infos_award;column:award;type:bigint unsigned" json:"award"`
-	AwardInfos AwardInfos `gorm:"joinForeignKey:award;foreignKey:id" json:"award_infos_list"`
+	AwardInfos AwardInfos `gorm:"foreignkey:Award;association_foreignkey:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Lottery    uint64     `gorm:"index:idx_winning_infos_lottery;column:lottery;type:bigint unsigned" json:"lottery"`
-	Lotteries  Lotteries  `gorm:"joinForeignKey:lottery;foreignKey:id" json:"lotteries_list"`
+	Lotteries  Lotteries  `gorm:"foreignkey:Lottery;association_foreignkey:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Address    string     `gorm:"column:address;type:tinytext" json:"address"`
-	Handout    bool       `gorm:"column:handout;type:tinyint(1)" json:"handout"`
+	Handout    bool       `gorm:"column:handout;type:tinyint(1);default:0" json:"handout"`
 }
 // TableName get sql table name.
 func (m *WinningInfos) TableName() string {
 	return "winning_infos"
 }
 
-
 //LotteryInfoRes: struct for LotteryInfo()
 type LotteryInfoRes struct {
-	LotteryItems 	[]Lotteries	`json:"lottery_items"`
-	Page 			uint64 		`json:"page"`
-	Rows 			uint64		`json:"rows"`
-	Total			int64		`json:"total"`
+	LotteryItems []Lotteries `json:"lottery_items"`
+	Page         uint64      `json:"page"`
+	Rows         uint64      `json:"rows"`
+	Total        int64       `json:"total"`
 }
+
 //Get a list of ongoing lottery
 func LotteryInfo(c *gin.Context) {
 	request := struct {
@@ -128,8 +129,8 @@ func LotteryInfo(c *gin.Context) {
 	rows := request.Rows
 	db := utils.GetMysql()
 
-	var lotteries 			[]Lotteries
-	var lotteryCount		int64
+	var lotteries []Lotteries
+	var lotteryCount int64
 	if err := db.Table("lotteries").Where("end_time > NOW()").Find(&lotteries).Count(&lotteryCount).Error; err != nil {
 		c.Status(http.StatusNotFound)
 		return
@@ -150,32 +151,33 @@ func LotteryInfo(c *gin.Context) {
 	c.JSON(http.StatusOK, res)
 }
 
-
 type AwardItem struct {
-	ID				uint64	`json:"award_id"`
-	Name			string	`json:"award_name"`
-	Description		string	`json:"award_description"`
-	Pic 			string	`json:"pic"`
-	Total 			uint64	`json:"total"`
-	DisplayRate		uint64	`json:"display_rate"`
-	Value 			uint64	`json:"value"`
+	ID          uint64 `json:"award_id"`
+	Name        string `json:"award_name"`
+	Description string `json:"award_description"`
+	Pic         string `json:"pic"`
+	Total       uint64 `json:"total"`
+	DisplayRate uint64 `json:"display_rate"`
+	Value       uint64 `json:"value"`
 }
+
 //AwardInfoRes: struct for AwardsInfo()
 type AwardInfoRes struct {
-	LotteryId			uint64		`json:"lottery_id"`
-	LotteryTitle		string		`json:"lottery_title"`
-	LotteryDescription	string		`json:"lottery_description"`
-	Awards				[]AwardItem	`json:"awards"`
-	Page 				uint64 		`json:"page"`
-	Rows 				uint64		`json:"rows"`
-	Total				int64		`json:"total"`
+	LotteryId          uint64      `json:"lottery_id"`
+	LotteryTitle       string      `json:"lottery_title"`
+	LotteryDescription string      `json:"lottery_description"`
+	Awards             []AwardItem `json:"awards"`
+	Page               uint64      `json:"page"`
+	Rows               uint64      `json:"rows"`
+	Total              int64       `json:"total"`
 }
+
 //Get prize information for lottery
 func AwardsInfo(c *gin.Context) {
 	request := struct {
 		LotteryId uint64 `json:"lottery_id"`
-		Page uint64 `json:"page"`
-		Rows uint64 `json:"rows"`
+		Page      uint64 `json:"page"`
+		Rows      uint64 `json:"rows"`
 	}{}
 	if c.ShouldBindJSON(&request) != nil {
 		c.Status(http.StatusBadRequest)
@@ -206,15 +208,15 @@ func AwardsInfo(c *gin.Context) {
 	awardsInfos = awardsInfos[(page-1)*rows : flag]
 
 	var awardsRequest []AwardItem
-	var t				AwardItem
+	var t AwardItem
 	for _, v := range awardsInfos {
-		t.ID 			= v.ID
-		t.Name			= v.Name
-		t.Description	= v.Description
-		t.Pic			= v.Pic
-		t.Total			= v.Total
-		t.DisplayRate	= v.DisplayRate
-		t.Value			= v.Value
+		t.ID = v.ID
+		t.Name = v.Name
+		t.Description = v.Description
+		t.Pic = v.Pic
+		t.Total = v.Total
+		t.DisplayRate = v.DisplayRate
+		t.Value = v.Value
 		awardsRequest = append(awardsRequest, t)
 	}
 	res := AwardInfoRes{
@@ -229,36 +231,36 @@ func AwardsInfo(c *gin.Context) {
 	c.JSON(http.StatusOK, res)
 }
 
-
 type WinItem struct {
-		Lottery		uint64	`json:"lottery_id"`
-		Title		string	`json:"lottery_title"`
-		AwardId		uint64	`json:"award_id"`
-		Name		string	`json:"award_name"`
-		Address 	string	`json:"address"`
-		Handout		bool	`json:"handout"`
+	Lottery uint64 `json:"lottery_id"`
+	Title   string `json:"lottery_title"`
+	AwardId uint64 `json:"award_id"`
+	Name    string `json:"award_name"`
+	Address string `json:"address"`
+	Handout bool   `json:"handout"`
 }
 type WinningInfoRes struct {
-	UserId 	uint64		`json:"user_id"`
-	Awards	[]WinItem	`json:"awards"`
-	Page 	uint64 		`json:"page"`
-	Rows 	uint64		`json:"rows"`
-	Total	int64		`json:"total"`
+	UserId uint64    `json:"user_id"`
+	Awards []WinItem `json:"awards"`
+	Page   uint64    `json:"page"`
+	Rows   uint64    `json:"rows"`
+	Total  int64     `json:"total"`
 }
+
 //Get the user winning information of the lottery
 func WinInfo(c *gin.Context) {
 	request := struct {
 		UserId uint64 `json:"user_id"`
-		Page uint64 `json:"page"`
-		Rows uint64 `json:"rows"`
+		Page   uint64 `json:"page"`
+		Rows   uint64 `json:"rows"`
 	}{}
 	if c.ShouldBindJSON(&request) != nil {
 		c.Status(http.StatusBadRequest)
 		return
 	}
-	userId 	:= request.UserId
-	page 	:= request.Page
-	rows 	:= request.Rows
+	userId := request.UserId
+	page := request.Page
+	rows := request.Rows
 
 	var winningCount int64
 	var winRes []WinItem
@@ -291,23 +293,23 @@ func WinInfo(c *gin.Context) {
 	c.JSON(http.StatusOK, res)
 }
 
-
 type returnType struct {
-	Permanent	uint64	`json:"permanent"`
-	Temporary	uint64	`json:"temporary"`
+	Permanent uint64 `json:"permanent"`
+	Temporary uint64 `json:"temporary"`
 }
+
 //Query user's remaining lottery draws
 func DrawTimes(c *gin.Context) {
 	request := struct {
-		UserId 		uint64  `json:"user_id"`
-		LotteryId	uint64	`json:"lottery_id"`
+		UserId    uint64 `json:"user_id"`
+		LotteryId uint64 `json:"lottery_id"`
 	}{}
 	if c.ShouldBindJSON(&request) != nil {
 		c.Status(http.StatusBadRequest)
 		return
 	}
-	userId 		:= request.UserId
-	lotteryId	:= request.LotteryId
+	userId := request.UserId
+	lotteryId := request.LotteryId
 
 	ctx := context.Background()
 	if rdb == nil {
@@ -316,7 +318,7 @@ func DrawTimes(c *gin.Context) {
 	}
 
 	var toReturn returnType
-	ans, err := rdb.Get( ctx, "remain:" + strconv.Itoa(int(lotteryId)) + ":" + strconv.Itoa(int(userId)) ).Result()
+	ans, err := rdb.Get(ctx, "remain:"+strconv.Itoa(int(lotteryId))+":"+strconv.Itoa(int(userId))).Result()
 	if err != nil {
 		// not found in redis, create one and insert into redis
 		lottery, flag := QueryLotteryById(lotteryId)
@@ -335,13 +337,14 @@ func DrawTimes(c *gin.Context) {
 		timesLeft := &myProto.UserTimes{}
 		timesLeft.Permanent = uint32(lottery.Permanent)
 		timesLeft.Temporary = uint32(lottery.Temporary)
-		timesLeft.Update	= ptypes.TimestampNow()
+		timesLeft.Update = ptypes.TimestampNow()
 		// save to redis
-		buffer, err := proto.Marshal(timesLeft); if err != nil {
+		buffer, err := proto.Marshal(timesLeft)
+		if err != nil {
 			c.Status(http.StatusInternalServerError)
 			return
 		}
-		rdb.Set(ctx, "remain:" + strconv.FormatUint(lotteryId, 10) + ":" + strconv.FormatUint(userId, 10), buffer, 0)
+		rdb.Set(ctx, "remain:"+strconv.FormatUint(lotteryId, 10)+":"+strconv.FormatUint(userId, 10), buffer, 0)
 	} else {
 		// found in redis
 		data := &myProto.UserTimes{}
@@ -353,27 +356,28 @@ func DrawTimes(c *gin.Context) {
 		toReturn.Temporary = uint64(data.Temporary)
 
 		// Update for the first time today
-		timeStamp0, _ 	:= getTimestamp()  // timestamp for 00:00:00 of today
-		timeStampRedis	:= data.Update.Seconds  // timestamp for last update
+		timeStamp0, _ := getTimestamp()       // timestamp for 00:00:00 of today
+		timeStampRedis := data.Update.Seconds // timestamp for last update
 		if time.Unix(timeStampRedis, 0).Before(time.Unix(timeStamp0, 0)) {
-			lottery, flag := QueryLotteryById(lotteryId); if flag != 0 {
+			lottery, flag := QueryLotteryById(lotteryId)
+			if flag != 0 {
 				c.Status(http.StatusNotFound)
 				return
 			}
 			// update "temporary" and "update"
-			data.Temporary	= uint32(lottery.Temporary)
-			data.Update		= ptypes.TimestampNow()
-			buffer, err := proto.Marshal(data); if err != nil {
+			data.Temporary = uint32(lottery.Temporary)
+			data.Update = ptypes.TimestampNow()
+			buffer, err := proto.Marshal(data)
+			if err != nil {
 				c.Status(http.StatusInternalServerError)
 				return
 			}
-			rdb.Set(ctx, "remain:" + strconv.FormatUint(lotteryId, 10) + ":" + strconv.FormatUint(userId, 10), buffer, 0)
+			rdb.Set(ctx, "remain:"+strconv.FormatUint(lotteryId, 10)+":"+strconv.FormatUint(userId, 10), buffer, 0)
 			toReturn.Temporary = lottery.Temporary
 		}
 	}
 	c.JSON(http.StatusOK, toReturn)
 }
-
 
 func QueryLotteryById(lotteryId uint64) (Lotteries, uint64) {
 	db := utils.GetMysql()
@@ -388,7 +392,7 @@ func QueryLotteryById(lotteryId uint64) (Lotteries, uint64) {
 func judgePageRange(page, rows, cnt uint64) uint64 {
 	flag := page * rows
 	// out of range
-	if (page != 1 && flag > cnt ) || flag == 0 {
+	if (page != 1 && flag > cnt) || flag == 0 {
 		return 0
 	}
 	// return the maximum rows
